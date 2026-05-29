ebcaffd2358ff0d2750c1da3c2cf12c0ca1f87b2
neostream-io <hello@nestahub.ng>
2026-05-29 15:25:45 +0100

    Update AddressView.tsx

--- src/components/bundles/mobile/AddressView.tsx
+++ src/components/bundles/mobile/AddressView.tsx
@@ -142,6 +142,7 @@ export function AddressView({ onSubmit, onBack }: AddressViewProps) {
         setSelectedAddress(address);
         setPredictions([]);
         setQuery("");
+      }
     );
   };
 
